29e70fdc406d9a732d2ef925f0d417b1ee2d27f9
Gordon <gordon.zhang@coxautoinc.com>
2022-10-13 08:29:04 -0400

    add code comments

--- urlpoll.go
+++ urlpoll.go
@@ -32,6 +32,14 @@ type State struct {
 // StateMonitor maintains a map that stores the state of the URLs being
 // polled, and prints the current state every updateInterval nanoseconds.
 // It returns a chan State to which resource state should be sent.
+/*
+StateMonitor will loop forever, selecting on two channels: ticker.C and update.
+The select statement blocks until one of its communications is ready to proceed.
+When StateMonitor receives a tick from ticker.C, it calls logState to print the current state.
+When it receives a State update from updates, it records the new status in the urlStatus map.
+Notice that this goroutine owns the urlStatus data structure, ensuring that it can only be accessed sequentially.
+This prevents memory corruption issues that might arise from parallel reads and/or writes to a shared map.
+*/
 func StateMonitor(updateInterval time.Duration) chan<- State {
 	updates := make(chan State)
 	urlStatus := make(map[string]string)
@@ -76,12 +84,30 @@ func (r *Resource) Poll() string {
 	return resp.Status
 }
 
-// Sleep sleeps for an appropriate interval (dependent on error state)
-// before sending the Resource to done.
+/*
+Sleep calls time.Sleep to pause before sending the Resource to done.
+ The pause will either be of a fixed length (pollInterval) plus an additional delay proportional to the number of sequential errors (r.errCount).
+
+This is an example of a typical Go idiom: a function intended to run inside a goroutine takes a channel,
+upon which it sends its return value (or other indication of completed state).
+
+*/
 func (r *Resource) Sleep(done chan<- *Resource) {
 	time.Sleep(pollInterval + errTimeout*time.Duration(r.errCount))
 	done <- r
 }
+
+/*
+Each Poller receives Resource pointers from an input channel.
+In this program, the convention is that sending a Resource pointer on a channel passes ownership of the underlying data from the sender to the receiver.
+Because of this convention, we know that no two goroutines will access this Resource at the same time.
+This means we don't have to worry about locking to prevent concurrent access to these data structures.
+The Poller processes the Resource by calling its Poll method.
+It sends a State value to the status channel, to inform the StateMonitor of the result of the Poll.
+Finally, it sends the Resource pointer to the out channel.
+This can be interpreted as the Poller saying "I'm done with this Resource" and returning ownership of it to the main goroutine.
+Several goroutines run Pollers, processing Resources in parallel.
+*/
 
 func Poller(in <-chan *Resource, out chan<- *Resource, status chan<- State) {
 	for r := range in {
@@ -102,14 +128,30 @@ func main() {
 	for i := 0; i < numPollers; i++ {
 		go Poller(pending, complete, status)
 	}
-
-	// Send some Resources to the pending queue.
+	/*
+	   To add the initial work to the system, main starts a new goroutine that allocates and sends one Resource per URL to pending.
+	   The new goroutine is necessary because unbuffered channel sends and receives are synchronous.
+	   That means these channel sends will block until the Pollers are ready to read from pending.
+	   Were these sends performed in the main goroutine with fewer Pollers than channel sends,
+	   the program would reach a deadlock situation, because main would not yet be receiving from complete.
+	   Exercise for the reader: modify this part of the program to read a list of URLs from a file.
+	    (You may want to move this goroutine into its own named function.)
+	*/
 	go func() {
 		for _, url := range urls {
 			pending <- &Resource{url: url}
 		}
 	}()
 
+	/*
+		When a Poller is done with a Resource, it sends it on the complete channel.
+		This loop receives those Resource pointers from complete. For each received Resource,
+		it starts a new goroutine calling the Resource's Sleep method.
+		Using a new goroutine for each ensures that the sleeps can happen in parallel.
+		Note that any single Resource pointer may only be sent on either pending or complete at any one time.
+		This ensures that a Resource is either being handled by a Poller goroutine or sleeping, but never both simultaneously.
+		In this way, we share our Resource data by communicating.
+	*/
 	for r := range complete {
 		go r.Sleep(pending)
 	}
